f98c2f1300503dd50dde5cfab5e220421f9793a0
admin@goktel.net <admin@goktel.net>
2023-02-25 23:05:43 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "goktelcallapp_37930",
   url: "https://goktelcallapp_37930.botics.co",
   api: "https://goktelcallapp_37930.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
