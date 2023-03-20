7473988351a6657d6788cf7f1b1fe31bb7720533
Crowdbotics <engineering@crowdbotics.com>
2023-03-20 18:37:03 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,28 @@ export const globalOptions = {
   name: "goktelcallapp_37930",
   url: "https://goktelcallapp_37930.botics.co",
   api: "https://goktelcallapp_37930.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  undefined: {
+    CLIENT_ID: CLIENT_ID,
+    CLIENT_SECRET: CLIENT_SECRET,
+    SDK_KEY: SDK_KEY,
+    SDK_SECRET: SDK_SECRET,
+    REDIRECT_URI: REDIRECT_URI,
+    initialWeeklyList: initialWeeklyList,
+    initialWeeksList: initialWeeksList,
+    initialDaysList: initialDaysList,
+    initialOccurrencesList: initialOccurrencesList,
+    initialRecurrenceList: initialRecurrenceList,
+    initialMeetingSchedule: initialMeetingSchedule,
+    CHECKBOX_COLOR_FALSE: "lightgray",
+    CHECKBOX_COLOR_TRUE: "#24ebde",
+    recurrentMeeting: recurrentMeeting,
+    checkboxColor: checkboxColor,
+    styles: styles
   }
-};
+}
